3b18464962d48a4a58df2a0546c3a7ea6279d537
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2020-01-27 22:48:47 +0530

    new update

--- Trip_adviser_bs4.py
+++ Trip_adviser_bs4.py
@@ -1,10 +1,22 @@
 from bs4 import BeautifulSoup as bs
 import requests
-
-sourse = requests.get("https://www.tripadvisor.in/Hotel_Review-g616028-d619324-Reviews-Haveli_Hari_Ganga_by_Leisure_Hotels-Haridwar_Haridwar_District_Uttarakhand.html#REVIEWS").text
-
-soup = bs(sourse,'lxml')
-for review_temp in soup.find_all('div', class_="hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"):
-    review = review_temp.q.text
-    print(review)
-    print()
+import time
+urls = []
+with open('hotels.log','r') as f:
+    for lines in f:
+        l = lines.split()
+        if (l[-1].endswith("#REVIEWS")):
+            urls.append(l[-1])
+f.close()
+with open('ritesh.txt','w',encoding="ascii") as a:
+    for links in urls:
+        try:
+            sourse = requests.get(links).text
+            soup = bs(sourse,'lxml')
+            for review_temp in soup.find_all('div', class_="hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"):
+                review = review_temp.q.text
+                a.write(review)
+                a.write("\n")
+        except:
+            time.sleep(1000)
+a.close()
