990abaa3c2ee066d9ee7c1d55375d923ee04dc4b
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2020-01-27 23:00:45 +0530

    updated

--- Trip_adviser_bs4.py
+++ Trip_adviser_bs4.py
@@ -8,15 +8,17 @@ with open('hotels.log','r') as f:
         if (l[-1].endswith("#REVIEWS")):
             urls.append(l[-1])
 f.close()
-with open('ritesh.txt','w',encoding="ascii") as a:
+with open('ritesh.txt','w') as a:
     for links in urls:
         try:
             sourse = requests.get(links).text
             soup = bs(sourse,'lxml')
             for review_temp in soup.find_all('div', class_="hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"):
                 review = review_temp.q.text
+                print(review)
+                print()
                 a.write(review)
                 a.write("\n")
         except:
-            time.sleep(1000)
+            time.sleep(100)
 a.close()
