4488a96720aaa42a5fd1f229395103a7b034e56c
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2020-01-28 00:03:41 +0530

    update

--- Trip_adviser_bs4.py
+++ Trip_adviser_bs4.py
@@ -15,9 +15,26 @@ with open('ritesh.txt','w') as a:
             soup = bs(sourse,'lxml')
             for review_temp in soup.find_all('div', class_="hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"):
                 review = review_temp.q.text
+                rate_5 = review_temp.find_all('span',class_="ui_bubble_rating bubble_50")
+                if(rate_5!=[]):
+                    raat=5
+                rate_4 = review_temp.find_all('span',class_="ui_bubble_rating bubble_40")
+                if(rate_4!=[]):
+                    raat=4
+                rate_3 = review_temp.find_all('span',class_="ui_bubble_rating bubble_30")
+                if(rate_3!=[]):
+                    raat=3
+                rate_2 = review_temp.find_all('span',class_="ui_bubble_rating bubble_20")
+                if(rate_2!=[]):
+                    raat=2
+                rate_1 = review_temp.find_all('span',class_="ui_bubble_rating bubble_10")
+                if(rate_1!=[]):
+                    raat=1
+                
                 print(review)
                 print()
-                a.write(review)
+                re = str(review)+"    rating = "+str(raat)
+                a.write(re)
                 a.write("\n")
         except:
             time.sleep(100)
